cdd63ddb2ad30f836af297ef65fc3bb1ba238d2a
nkfa2013 <92278248+nkfa2013@users.noreply.github.com>
2024-11-29 19:43:15 -0300

    Criada classe Cargo
    - Atributo nome

---
+++ ts/cargo.ts
@@ -0,0 +1,16 @@
+export class Cargo {
+    private _nome: string;
+
+    constructor(nome: string) {
+        this._nome = nome;
+    }
+
+    public get nome() : string {
+        return this._nome;
+    }
+
+    public set nome(nome : string) {
+        this._nome = nome;
+    }
+    
+}
